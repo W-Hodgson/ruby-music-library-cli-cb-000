80b2fba0b7a40fe0515b4c19991147228daedfa5
Will Hodgson <will.hodgson@current-rms.com>
2019-08-23 08:05:47 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,63 @@
+class MusicLibraryController
+
+  def initialize(file_path="./db/mp3s")
+    MusicImporter.new(file_path).import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    case gets.chomp
+    when "list songs"
+      list_songs
+    when "list artists"
+      list_artists
+    when "list genres"
+      list_genres
+    when "list artist"
+      list_songs_by_artist
+    end
+  end
+
+  def list_songs
+    Song.list_songs
+  end
+
+  def list_artists
+    Artist.list_artists
+  end
+
+  def list_genres
+    Genre.list_genres
+  end
+
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist_name = gets.chomp
+    if Artist.find_by_name(artist_name)
+      artist = Artist.find_by_name(artist_name)
+      artist.list_songs_by_artist
+    end
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    genre_name = gets.chomp
+    if Genre.find_by_name(genre_name)
+      genre = Genre.find_by_name(genre_name)
+      genre.list_songs_by_genre
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    song_name = gets.chomp
+  end
+end
